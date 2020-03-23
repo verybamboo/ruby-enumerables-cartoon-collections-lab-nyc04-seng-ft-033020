58bac0a9c7e4ff26ca0ca31a916b49562168b9cd
George Chen <georgechen1338@gmail.com>
2020-03-23 22:24:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,6 +25,8 @@ def long_planeteer_calls(elements)# code an argument here
   elements.any? { |call| call.length > 4}
 end
 
+long_planeteer_calls(elements)
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
