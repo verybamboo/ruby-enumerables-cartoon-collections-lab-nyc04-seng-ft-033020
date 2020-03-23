bf4da449e177d156011e8016cc5c2072c77efef4
George Chen <georgechen1338@gmail.com>
2020-03-23 22:25:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,6 +31,6 @@ def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
    cheese.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
+    cheese_types.include?(maybe_cheese)
   end
 end
